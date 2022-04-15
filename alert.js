6fbf4d7f2aca70854df542211f3174b2b76c109c
Laiba <lili.shaqil@gmail.com>
2022-04-16 00:59:16 +0500

    first comment

---
+++ alert.js
@@ -0,0 +1,438 @@
+// This is my first Chaper Code :
+
+// topic ------Alert-----
+ 
+// Qno1: 
+
+// alert("Hello Welcome to My Website");
+
+// Qno:02
+
+// alert("Error! Please enter a Valid passward");
+
+// Qno:03
+
+// alert("Welcome to JS land....\n Happy Coding!");
+
+// Qno:04
+
+// alert("Happy Coding! \n Prevent this page from craeting additional dialogs.");
+
+// Qno:05
+
+// alert("Hello... I can run JS through my web browser`s console.");
+
+// Qno:06 
+
+/* <body>
+    <script>alert("click here ! for my website")</script>
+    <h1>laiba shakeel</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+ Facere enim vitae soluta nesciunt reprehenderit, quis nihil voluptates officia modi
+  inventore deleniti. Voluptatum libero vel aliquid ea quam esse nobis perspiciatis.</p>
+</body> */
+
+// Qno:07
+
+// ( 07 part 1 )
+
+// ##HEAD sCRIPT##
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>javaScript</title>
+//     <script>alert("This is the Head Script")</script>
+// </head>
+// <body>
+    
+//     <h1>laiba shakeel</h1>
+//     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+//  Facere enim vitae soluta nesciunt reprehenderit, quis nihil voluptates officia modi
+//   inventore deleniti. Voluptatum libero vel aliquid ea quam esse nobis perspiciatis.</p>
+// </body>
+// </html>
+
+// ( 07 part 2 )
+
+// ## BEFORE-BODY_TAG ## 
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>javaScript</title>   
+// </head>
+// <script>alert("This is Before Body tag Script");</script>
+// <body>
+    
+//     <h1>laiba shakeel</h1>
+//     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+//  Facere enim vitae soluta nesciunt reprehenderit, quis nihil voluptates officia modi
+//   inventore deleniti. Voluptatum libero vel aliquid ea quam esse nobis perspiciatis.</p>
+// </body>
+// </html>
+
+// ( 07 part 3 )
+
+// ## INSIDE-BODY_TAG ## 
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>javaScript</title>   
+// </head>
+// <body>
+//     <script>alert("This is Inside Body tag Script");</script>
+//     <h1>laiba shakeel</h1>
+//     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+//  Facere enim vitae soluta nesciunt reprehenderit, quis nihil voluptates officia modi
+//   inventore deleniti. Voluptatum libero vel aliquid ea quam esse nobis perspiciatis.</p>
+// </body>
+// <script src="./alert.js"></script>
+// </html>
+
+// ( 07 part 4 )
+
+// ## AFTER-BODY_TAG ## 
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>javaScript</title>   
+// </head>
+// <body>
+//     <h1>laiba shakeel</h1>
+//     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+//  Facere enim vitae soluta nesciunt reprehenderit, quis nihil voluptates officia modi
+//   inventore deleniti. Voluptatum libero vel aliquid ea quam esse nobis perspiciatis.</p>
+// </body>
+// <script>alert("This is After Body tag Script");</script>
+// <script src="./alert.js"></script>
+// </html>
+
+// ------------------------------------------------------------------------------
+
+// CHAPTER : 02 "START"
+
+// TOPI NAME : VARIABLES FOR STRING 
+
+// Qno :01 & 02
+
+// var username = "Laiba Shakeel Ahmed!"
+// alert(username);
+
+// Qno:03
+
+// var message = "Hello World!"
+// alert(message);
+
+
+// Qno :04
+
+// var name = "Johne Doe"
+// var age = "15 years old"
+// var work = "Certified Mobile Application Development"
+
+// alert(name);
+// alert(age);
+// alert(work);
+
+
+// Qno : 05 
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza);
+
+// Qno : 06 
+
+// var email = "lili.shaqil2gmail.com"
+// alert("My Email Adress is " + email);
+
+// Qno:07
+
+// var book = "A Smarter way\nto learn JavaScript"
+// alert("I am trying to learn from the Book " + book);
+
+// Qno:08
+
+// document.write("Yeah! I Can write HTML content through JavaScript");
+
+// Qno: 09
+
+// var x = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(x);
+
+
+
+// ---------------------------------------------------
+
+
+
+// CHAPTER NUM : 03 
+
+// TOPIC == VARIABLES FOR NUMBER:
+
+// Qno: 01
+
+// var age = "17 years old"
+// alert("I am " + age);
+
+
+// Qno :02 
+
+// var visit_1 = "You Have visited this site 1 times"
+// var visit_2 = "You Have visited this site 2 times"
+// var visit_3 = "You Have visited this site 3 times"
+// var visit_4 = "You Have visited this site 4 times"
+// var visit_5 = "You Have visited this site 5 times"
+// var visit_6 = "You Have visited this site 6 times"
+// var visit_7 = "You Have visited this site 7 times"
+// var visit_8 = "You Have visited this site 8 times"
+// var visit_9 = "You Have visited this site 9 times"
+// var visit_10 = "You Have visited this site 10 times"
+// var visit_11 = "You Have visited this site 11 times"
+// var visit_12 = "You Have visited this site 12 times"
+// var visit_13 = "You Have visited this site 13 times"
+
+// alert(visit_1);
+// alert(visit_2);
+// alert(visit_3);
+// alert(visit_4);
+// alert(visit_5);
+// alert(visit_6);
+// alert(visit_7);
+// alert(visit_8);
+// alert(visit_9);
+// alert(visit_10);
+// alert(visit_11);
+// alert(visit_12);
+// alert(visit_13);
+// alert(visit_14);
+
+
+// Qno : 03
+
+// document.write("My Birthday year is 1990 </br> Data Type of my Decleard Variable is number");
+
+// Qno :04
+
+// document.write("<b>John Doe</b> ordered <b>5 T-Shirts</b>(s) on XYZ CLOTING STORE");
+
+// --------------------------------------------
+
+// CHAPTER NUM :05 
+
+// TOPIC: VARIABLE NAME: LEAGAL ||| ILLEGAL 
+
+// Qno : 01
+
+// var x = " my name "
+// var y = "is Laiba "
+// var z = "Shakeel Ahmed"
+// alert(x + y + z);
+
+// Qno: 02 
+
+// 5 legal Variable name
+
+// 1 ) var name 
+// 2)  var _name_space
+// 3)  var nameSpace 
+// 4)  var nameSpace1
+// 5)  var nameSpace_01 
+
+
+// 5 Illegal variabme name
+
+// 1 ) var  java script
+// 2)  var  12java
+// 3)  var  */java
+// 4)  var  Java-script
+// 5)  var   $java
+
+// Qno :03 
+
+// document.write("<h1>Rules for naming JS variables </h1> \n\n\n Varibale name can only containe, numbers, $ and __ . For example: $my_1stVariable. </br></br> Variable must begin with a Letter, $ or ___ For example : $name, _name or name </br></br> Variable names are case sensitive </br></br> Varible names should not be JS keywords ")
+
+
+// Qno :04 
+
+// CHAPTER NUM :05 
+
+// TOPIC: MATH EXPRESSION:
+
+// Qno : 1
+
+// var x  = 5;
+// var y  = 3;
+// var sum = x + y;
+// console.log('The sum of ' + x  + ' and ' +  y + ' is: ' + sum);
+
+// // Qno :02 
+
+// var x  = 5;
+// var y  = 3;
+// var sub = x - y;
+// console.log(sub);
+
+
+// var x  = 5;
+// var y  = 3;
+// var mul = x * y;
+// console.log(mul);
+
+
+// var x  = 5;
+// var y  = 3;
+// var div = x / y;
+// console.log(div);
+
+// var x  = 5;
+// var y  = 3;
+// var Mod = x % y;
+// console.log(Mod);
+
+// Qno:03 
+
+// var a = 5;
+// var b = 7;
+// var c = a+b
+// document.write("Initialize value: " + a );
+// document.write("<br>");
+// document.write("Value after increment : " + ++a  );
+// document.write("<br>");
+// document.write("Value after addidtion : " + ++c);
+// document.write("<br>");
+// document.write("Value after decrement : " + --c);
+// document.write("<br>");
+// document.write("Remainder is: " + c%3);
+
+// Qno :04 
+
+// var Movie_ticket = 600;
+// var ticket_Rate = 5;
+// document.write("Total cost to buy 5 tickets to a movie is " + Movie_ticket * ticket_Rate);
+
+// Qno : 05
+
+
+
+// Qno: 06 
+
+// var F = 70;
+//  var C = 25;
+//  var Celcius = (F-32) * 5/9;
+//  document.write("25C is: " + Celcius);
+// document.write("<br>");
+// var Fahrenheit = (C*9/5) + 32;
+// document.write("70F is: " + Fahrenheit);
+
+// Qno : 07 
+
+
+// document.write("<h1><u>Shopping Cart</u></h1>")
+// document.write("<br>");
+// var price_1st_thing = 650;
+// var quantity_1st_thing = 3;
+// var price_2nd_thing = 100;
+// var quantity_2nd_thing = 7;
+// var shipping_charges = 100;
+// var total_cost =(price_1st_thing * quantity_1st_thing + price_2nd_thing * quantity_2nd_thing + shipping_charges);
+// document.write(" <b>Price of 1st thing  is :</b> " + price_1st_thing );
+// document.write("<br>");
+// document.write("<b>Quantity of 1st thing is : </b> " + quantity_1st_thing);
+// document.write("<br>");
+// document.write("<b>Price of 2nd thing is : </b>" + price_2nd_thing);
+// document.write("<br>");
+// document.write(" <b>Quantity of 2nd thing is :</b> " + quantity_2nd_thing );
+// document.write("<br>");
+// document.write(" <b>The Shipping Charges is : </b>" + shipping_charges);
+// document.write("<br>");
+// document.write(" <b>Total Cost of your order is : </b>" + total_cost);
+
+// Qno : 08 
+
+// var urdu = 85;
+// var english = 80;
+// var physics = 90;
+// var maths = 99;
+// var computer = 100;
+// var totalMarks = 500;
+// var Marks_obtained = (urdu + english + physics + maths + computer);
+// var percentage = (Marks_obtained/totalMarks)*100;
+// document.write("<h1><u>MarkSheet</u></h1>")
+// document.write("<br>");
+// document.write("Total Marks: "+totalMarks);
+// document.write("<br>");
+// document.write("Marks Obtained: "+Marks_obtained);
+// document.write("<br>");
+// document.write("Percentage: "+ percentage);
+
+// Qno:09 
+
+// var US_Dollar = 104.80;
+// var Saudi_Riyals = 28;
+// var total_Currency = (US_Dollar*10+Saudi_Riyals*25);
+// document.write("<h1><u>Currency in PKR</u></h1>")
+// document.write("<br>");
+// document.write("<b>Total Currency in PKR : </b>" + total_Currency)
+
+// Qno :10
+
+// var num = 10;
+// var result = (num + 5 )* 10 / 2 ;
+// document.write("<h1><u>Calculation</u></h1>")
+// document.write("<br>");
+// document.write("The result is:" + result);
+
+// Qno : 11
+
+// document.write(" Age Calculator ");
+// document.write("<br>");
+// document.write("<br>");
+// var current_year = 2022;
+// var birth_year = 2003;
+// var Current_age = current_year - birth_year;
+// document.write("<h1><u>Age Calculator</u></h1>")
+// document.write("<br>");
+// document.write("Your Current Age is: " + Current_age);
+
+// Qno : 12
+
+// document.write("<h1>The Geometrizer</h1>");
+// document.write("<br>");
+// var radius_value = 20;
+// var pi_value = 3.142;
+// var C = 2*pi_value*radius_value;
+// var area = pi_value*radius_value*radius_value;
+// document.write("1) The Radius of the Circle is =  " +  radius_value);
+// document.write("</br></br>");
+// document.write("2) Circumference of circle = " + C);
+// document.write("</br></br>");
+// document.write("3) Area of circle = " + area);
+
+
+// Qno: 13
+
+// var snacks = "lays";
+// var age = 15;
+// var max_age = 65;
+// var estimated_amt = 3;
+// var totalNeeded = estimated_amt*(max_age-age);
+// var message = (" You will need " + totalNeeded +" "+ snacks + " to last you until the ripe old age of " + max_age );
+// document.write(message);
+
+// ----------------------------------------------------------
